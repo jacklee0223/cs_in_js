a3d6b886df3d15c917b75e3276a7d5284be6b269
Jeong Min Lee <jacklee0223@gmail.com>
2018-02-19 22:29:42 -0800

    add comments

--- brainteaser/reverse_integer.js
+++ brainteaser/reverse_integer.js
@@ -1,14 +1,14 @@
 function reverseInteger(n) {
-  // Write your code here, and
-  // return your final answer.
-  const num = 0;
-
-  while (n !== 0) {
-    num = num * 10 + n % 10;
+  // define result
+  let result = 0;
+  // while n is > 0
+  while (n > 0) {
+    // multiply result by 10, and add remainder of n%10
+    result = result * 10 + n % 10;
+    // set n to n/10 floor
     n = Math.floor(n / 10);
   }
-
-  return num;
+  return result;
 }
 
 reverseInteger(4321);
