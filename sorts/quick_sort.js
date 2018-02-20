a27ae5fa089483be656f44e7ab1e9a69a7b8e91f
Jeong Min Lee <jacklee0223@gmail.com>
2018-02-20 14:10:47 -0800

    refine quicksort with es6 syntax

--- sorts/quick_sort.js
+++ sorts/quick_sort.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const array = [ 1, 4, 1, 3, 5, 6, 4, 8 ];
+const array = [1, 4, 1, 3, 5, 6, 4, 8];
 const obj = { foo: 'bar', fooz: 'baz' };
 const nothing = null;
 const empty_array = [];
@@ -9,36 +9,27 @@ const empty_array = [];
 than pivot into left_array, and elements bigger than pivot into right_array, 
 call quick_sort itself on the left_array and right_array, finally returning sorted 
 and concatenated array from left_array, pivot, and right_array */
-function quick_sort( input_array ) {
-    // If input_array is not an array, return
-    if ( !Array.isArray( input_array ) ) {
-        return "Not an array";
-    }
-    // Base case: If there is nothing in input_array, return empty array
-    if ( input_array.length === 0 ) {
-        return [];
-    }
-    // Define pivot, left_array, and right_array
-    const pivot = input_array[ 0 ];
-    const left_array = [];
-    const right_array = [];
-    /* loop through input_array to push values smaller than pivot into 
-    left_array and bigger than pivot into right_array */
-    for ( let i=1; i<input_array.length; i++ ) {
-        if ( input_array[i] <= pivot ) {
-            left_array.push( input_array[i] );
-        } else if ( input_array[i] > pivot ) {
-            right_array.push( input_array[i] );
-        }
-    }
-    /* Recursive case: return the concatenated array containing all values in 
-    fully sorted left_array, pivot, and fully sorted right_array 
-    (fully sort by calling quick_sort itself on left_array and right_array) */
-    return quick_sort(left_array).concat( pivot, quick_sort(right_array) );
+const array = [5, 2, 1, 3, 4, 2, 4, 8, 10];
+
+function quick_sort(arr) {
+  // if not array, return
+  if (!Array.isArray(arr)) return 'Not array';
+  // if arr.length < 2 return arr
+  if (arr.length < 2) return arr;
+  // pick pivot, initialize left and right array
+  const pivot = arr[0],
+    left = [],
+    right = [];
+  // iterate through arr, put number smaller than pivot to left, larger than pivot to right
+  for (let i = 1; i < arr.length; i++) {
+    arr[i] < pivot ? left.push(arr[i]) : right.push(arr[i]);
+  }
+  // call function itself on left and right until they are all sorted and concat left, pivot, and right together to form single fully sorted array
+  return quick_sort(left).concat(pivot, quick_sort(right));
 }
 
 quick_sort(array);
-console.log(quick_sort( array ));
-console.log(quick_sort( obj ));
-console.log(quick_sort( nothing ));
-console.log(quick_sort( empty_array ));
+console.log(quick_sort(array));
+console.log(quick_sort(obj));
+console.log(quick_sort(nothing));
+console.log(quick_sort(empty_array));
