3af484747263d8e7f03c603907dcd703333e04d4
Jeong Min Lee <jacklee0223@gmail.com>
2018-10-15 09:38:45 -0700

    update nth_prime

--- brainteaser/nth_prime.js
+++ brainteaser/nth_prime.js
@@ -1,16 +1,23 @@
 function isPrime (num) {
-  for(let i = 2; i < num; i++)
+  for(let i = 2; i <= Math.floor(Math.sqrt(num)); i++)
     if(num % i === 0) return false;
   return num !== 1 && num !== 0;
 }
 
 function nthPrime (n) {
-  const result = [];
+  let result = 3;
 
-  for ( let i=3; i<=n; i++ ) {
-    if ( isPrime(i) ) result.push(i)
-  }
-  return result.length;
+    for (var i = 1; 0 < n; i++) {
+      if(isPrime(i)) {
+        result = i; n--;
+      }
+      // we can skip the even numbers
+      if(3 <= i){
+        i++;
+      }
+    }
+
+  return result;
 }
 
 nthPrime(10001)
