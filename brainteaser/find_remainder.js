388430010c085c5adf5644baa54f074756ffe843
Jeong Min Lee <jacklee0223@gmail.com>
2018-10-15 13:06:28 -0700

    fix find remainder

--- brainteaser/find_remainder.js
+++ brainteaser/find_remainder.js
@@ -1,16 +1,15 @@
 function find_remainder(n, divisor) {
   if ( divisor === 0 || isNaN(n) || isNaN(divisor) ) return NaN;
 
-  const nAbs = Math.abs(n), divAbs = Math.abs(divisor);
-  let remainder;
+  let nAbs = Math.abs(n), divAbs = Math.abs(divisor);
 
   while ( nAbs >= divAbs ) {
-    remainder = nAbs - divAbs;
+    nAbs -= divAbs;
   }
 
-  return n > 0 ? remainder : -remainder
+  return n > 0 ? nAbs : -nAbs
 }
 
-// find_remainder(10, 5)
-// find_remainder(9, 2)
-// find_remainder(27, 5)
+// find_remainder(10, 5) // 0
+// find_remainder(9, 2) // 1
+find_remainder(27, 5) // 2
