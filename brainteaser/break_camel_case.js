6b231cd89f0fb9b775177cfcff24ec1d1fbe81cb
Jeong Min Lee <jacklee0223@gmail.com>
2018-10-15 22:12:02 -0700

    implement break camel case

---
+++ brainteaser/break_camel_case.js
@@ -0,0 +1,14 @@
+// complete the function
+function solution(string) {
+  let result = ''
+
+  for ( let i=0; i<string.length; i++ ) {
+    if ( string[i] === string[i].toUpperCase() ) {
+      result += ` ${string[i]}`
+    } else {
+      result += string[i]
+    }
+  }
+
+  return result;
+}
